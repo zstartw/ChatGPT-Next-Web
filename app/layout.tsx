09e12fe9837753c00abf96d46832f034126904e2
Just do it <betterzw@gmail.com>
2023-05-09 07:47:53 +0000

    文案修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Suncent",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Suncent",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
