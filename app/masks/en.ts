795f3e70b3eddd49f07c0140ec410d0bb46e5958
Just do it <betterzw@gmail.com>
2023-05-26 15:04:39 +0800

    代码优化

--- app/masks/en.ts
+++ app/masks/en.ts
@@ -13,7 +13,7 @@ export const EN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       temperature: 0.3,
       max_tokens: 2000,
       presence_penalty: 0,
@@ -53,7 +53,7 @@ export const EN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       temperature: 0.5,
       max_tokens: 2000,
       presence_penalty: 0,
@@ -104,7 +104,7 @@ export const EN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       temperature: 0.5,
       max_tokens: 2000,
       presence_penalty: 0,
